88e1c5d2d9a0701a658dd33cbd4e6374dfe64f11
Juan Bancalari <juanbancalari11@gmail.com>
2022-07-16 00:48:49 +0200

    1er commit trabajo con Jona

---
+++ AppIntermedio/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Libro, Ubicacion
+
+# Register your models here.
+admin.site.register(Libro)
+admin.site.register(Ubicacion)
+
---
+++ AppIntermedio/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def libros(request):
+    return render(request,"libros/index.html")
+
+def inicio(request):
+    return render(request,"AppIntermedio/inicio.html")
+
+def crear(request):
+    return render(request,"libros/crear.html") 
+
+def editar(request):
+    return render(request,"libros/editar.html")
+
+def ubicacion(request):
+    return render(request, "ubicacion/buscar.html")
+
+def buscar(request):
+    return render(request, "ubicacion/buscar.html")
+
