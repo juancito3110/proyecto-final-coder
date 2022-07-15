4138d9113c7c7fa6b9d7e0d8a2006277c654d926
Jonx76 <jonathanmino@icloud.com>
2022-07-15 10:18:29 -0300

    3er commit

--- intermedio/AppIntermedio/models.py
+++ intermedio/AppIntermedio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Libro(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Título')
     imagen = models.ImageField(upload_to='imagenes/', verbose_name="Imagen", null=True)
     descripcion = models.TextField(verbose_name="Descripción",null=True)
